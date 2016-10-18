e83a4b8798d70a61fbf776178b2b5d9bd75902ed
wd103625 <wd103625@alibaba-inc.com>
2016-10-18 23:52:52 +0800

    a

---
+++ src/com/autonavi/git/GitDemo.java
@@ -0,0 +1,14 @@
+package com.autonavi.git;
+
+/**
+ * @author wd103625
+ * @date 2016年10月18日 下午11:50:55
+ * 
+ */
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.err.println("test");
+	}
+
+}

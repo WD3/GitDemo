9f02a6d9467db448e8eb21e3859b65987b3dde30
wd103625 <wd103625@alibaba-inc.com>
2016-10-19 00:12:07 +0800

    啊

--- src/com/autonavi/git/GitDemo1.java
+++ src/com/autonavi/git/GitDemo1.java
@@ -2,13 +2,13 @@ package com.autonavi.git;
 
 /**
  * @author wd103625
- * @date 2016年10月18日 下午11:50:55
+ * @date 2016骞�10鏈�18鏃� 涓嬪崍11:50:55
  * 
  */
 public class GitDemo1 {
 
 	public static void main(String[] args) {
-		System.err.println("远程增加");
+		System.err.println("本地减少");
 	}
 
 }

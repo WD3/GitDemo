b86fc057a192a172290b3601c23a5db0a5e5a231
WD3 <xingzhe1949@163.com>
2016-10-19 00:13:07 +0800

    Create GitDemo3.java

---
+++ src/com/autonavi/git/GitDemo3.java
@@ -0,0 +1,14 @@
+package com.autonavi.git;
+
+/**
+ * @author wd103625
+ * @date 2016Äê10ÔÂ18ÈÕ ÏÂÎç11:50:55
+ * 
+ */
+public class GitDemo3 {
+
+	public static void main(String[] args) {
+		System.err.println("远程增加");
+	}
+
+}
